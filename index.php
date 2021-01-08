1242dcb40069da46f38c29bec542fa370cdc0c5d
Kari Thomas <karithomas@Karis-MacBook-Air.local>
2021-01-08 15:43:51 -0500

    Updated homepage

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php include('/var/www/templates/h_open.php'); ?>
 </head>
 <body>
-    <h1>index.html</h1>
+    
+<header>
+    <h1 class="title">Kari Thomas</h1>
+    <h2 class="subtitle">FantasyPros Dev Trainee</h2>
+</header> 
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/sports-fandom.html">Sports Fandom</a></li>
+        <li><a href="/table.html">Table Goes Here</a></li>
+        <li><a href="/contact.html">Contact Me</a></li>
+        <li><a href="/sports-structure.html">League Info</a></li>
+    </ul>
+</nav>
+<main>
+    <h1>About Me</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum dolores autem quam voluptates deserunt similique sint soluta sed blanditiis dolorum ea impedit, maxime, ratione quisquam corporis laudantium distinctio veniam dicta!</p>
+    <p>Dolor accusamus optio reiciendis sed, perspiciatis ea eum facere expedita dolorum cum enim quo magnam eos laborum incidunt fugiat non aut sapiente est. Iure nostrum, doloribus neque sint provident repellendus.</p>
+    <p>Quos voluptate dolorem ad nostrum autem rerum quae itaque, sequi impedit necessitatibus illum eveniet, cumque sunt architecto similique vel! Odio aperiam fugiat labore veritatis. Explicabo omnis cumque assumenda iste officiis!</p>
+</main>
+
 </body>
 </html>
