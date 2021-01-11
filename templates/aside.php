6121411907ddad368b6e95c08a670af87b2c12ab
Kari Thomas <karithomas@Karis-MacBook-Air.local>
2021-01-10 21:08:00 -0500

    Fixed Contact Me section

--- templates/aside.php
+++ templates/aside.php
@@ -2,8 +2,10 @@
     <h2>Contact Me</h2>
     <address>
         <dl>
-            <dt>Phone Number:</dt><dd><a href-"tel:1-804-229-5551">(804) 229-5551</a></dd>
-            <dt>E-mail:</dt><dd><a href="mailto:kari@fantasypros.com">kari@fantasypros.com</a></dd>
+            <dt>Phone Number:</dt>
+            <dd><a href="tel:1-804-229-5551">(804) 229-5551</a></dd>
+            <dt>E-mail:</dt>
+            <dd><a href="mailto:kari@fantasypros.com">kari@fantasypros.com</a></dd>
         </dl>
     </address>
 </aside>
