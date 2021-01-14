0eab990d344d925f52edcfbbd90e0f37f731e4f9
Kari Thomas <karithomas@Karis-MacBook-Air.local>
2021-01-14 16:19:01 -0500

    Fixed sports-structure page for project

--- sports-structure.php
+++ sports-structure.php
@@ -4,7 +4,229 @@ include 'templates/h_open.php';
 include 'templates/h_close.php';
 ?>
     <h1>Sports Structure</h1>
-        <h2>NFL</h2>
+
+    <ul>
+        <li>NFL
+            <ul>
+                <li>NFC
+                    <ul>
+                        <li>East
+                            <ul>
+                                <li>Washington Football Team</li>
+                                <li>Philadelphia Eagles</li>
+                                <li>Dallas Cowboys</li>
+                                <li>New York Giants</li>
+                            </ul>
+                        </li>
+                        <li>West
+                            <ul>
+                                <li>Seattle Seahawks</li>
+                                <li>Los Angeles Rams</li>
+                                <li>Arizona Cardinals</li>
+                                <li>San Francisco 49ers</li>
+                            </ul>
+                        </li>
+                        <li>North
+                            <ul>
+                                <li>Green Bay Packers</li>
+                                <li>Chicago Bears</li>
+                                <li>Minnesota Vikings</li>
+                                <li>Detroit Lions</li>
+                            </ul>
+                        </li>
+                        <li>South
+                            <ul>
+                                <li>New Orleans Saints</li>
+                                <li>Tampa Bay Buccaneers</li>
+                                <li>Carolina Panthers</li>
+                                <li>Atlanta Falcons</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>AFC
+                    <ul>
+                        <li>East
+                            <ul>
+                                <li>Buffalo Bills</li>
+                                <li>Miami Dolphins</li>
+                                <li>New England Patriots</li>
+                                <li>New York Jets</li>
+                            </ul>
+                        </li>
+                        <li>West
+                            <ul>
+                                <li>Kansas City Chiefs</li>
+                                <li>Las Vegas Raiders</li>
+                                <li>Los Angeles Chargers</li>
+                                <li>Denver Broncos</li>
+                            </ul>
+                        </li>
+                        <li>North
+                            <ul>
+                                <li>Pittsburgh Steelers</li>
+                                <li>Baltimore Ravens</li>
+                                <li>Cleveland Browns</li>
+                                <li>Cincinnati Begnals</li>
+                            </ul>
+                        </li>
+                        <li>South
+                            <ul>
+                                <li>Tennessee Titans</li>
+                                <li>Indianapolis Colts</li>
+                                <li>Houston Texans</li>
+                                <li>Jacksonville Jaguars</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+    <ul>
+        <li>MLB
+            <ul>
+                <li>National League
+                    <ul>
+                        <li>East
+                            <ul>
+                                <li>Atlanta Braves</li>
+                                <li>Florida Marlins</li>
+                                <li>Philadelphia Phillies</li>
+                                <li>Washington Nationals</li>
+                                <li>New York Mets</li>
+                            </ul>
+                        </li>
+                        <li>Central
+                            <ul>
+                                <li>Chicago Cubs</li>
+                                <li>St. Louis Cardinals</li>
+                                <li>Cincinnati Reds</li>
+                                <li>Milwaukee Brewers</li>
+                                <li>Pittsburgh Pirates</li>
+                            </ul>
+                        </li>
+                        <li>West
+                            <ul>
+                                <li>Los Angeles Dodgers</li>
+                                <li>San Diego Padres</li>
+                                <li>San Francisco Giants</li>
+                                <li>Colorado Rockies</li>
+                                <li>Arizona Diamondbacks</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>American League
+                    <ul>
+                        <li>East
+                            <ul>
+                                <li>Tampa Bay Rays</li>
+                                <li>New York Yankees</li>
+                                <li>Toronto Blue Jays</li>
+                                <li>Baltimore Orioles</li>
+                                <li>Boston Red Sox</li>
+                            </ul>
+                        </li>
+                        <li>Central
+                            <ul>
+                                <li>Minnesota Twins</li>
+                                <li>Cleveland Indians</li>
+                                <li>Chicago White Sox</li>
+                                <li>Kansas City Royals</li>
+                                <li>Detroit Tigers</li>
+                            </ul>
+                        </li>
+                        <li>West
+                            <ul>
+                                <li>Oakland Athletics</li>
+                                <li>Houston Astros</li>
+                                <li>Seattle Mariners</li>
+                                <li>Los Angeles Angels</li>
+                                <li>Texas Rangers</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <ul>
+        <li>NBA
+            <ul>
+                <li>Eastern Conference
+                    <ul>
+                        <li>Atlantic
+                            <ul>
+                                <li>Boston Celtics</li>
+                                <li>Philadelphia 76ers</li>
+                                <li>Brooklyn Nets</li>
+                                <li>New York Knicks</li>
+                                <li>Toronto Raptors</li>
+                            </ul>
+                        </li>
+                        <li>Central
+                            <ul>
+                                <li>Milwaukee Bucks</li>
+                                <li>Indiana Pacers</li>
+                                <li>Cleveland Cavaliers</li>
+                                <li>Chicago Bulls</li>
+                                <li>Detroit Pistons</li>
+                            </ul>
+                        </li>
+                        <li>Southeast
+                            <ul>
+                                <li>Orlando Magic</li>
+                                <li>Charlotte Hornets</li>
+                                <li>Atlanta Hawks</li>
+                                <li>Miami Heat</li>
+                                <li>Washington Wizards</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>Western Conference
+                    <ul>
+                        <li>Northwest
+                            <ul>
+                                <li>Utah Jazz</li>
+                                <li>Portland Trail Blazers</li>
+                                <li>Denver Nuggets</li>
+                                <li>Oklahoma City Thunder</li>
+                                <li>Minnesota Timberwolves</li>
+                            </ul>
+                        </li>
+                        <li>Pacific
+                            <ul>
+                                <li>Los Angeles Lakers</li>
+                                <li>Los Angeles Clippers</li>
+                                <li>Phoenix Suns</li>
+                                <li>Golden State Warriors</li>
+                                <li>Sacramento Kings</li>
+                            </ul>
+                        </li>
+                        <li>Southwest
+                            <ul>
+                                <li>Dallas Mavericks</li>
+                                <li>San Antonio Spurs</li>
+                                <li>Memphis Grizzlies</li>
+                                <li>New Orleans Pelicans</li>
+                                <li>Houston Rockets</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+<?php include 'templates/aside.php'; ?>
+<?php include 'templates/f_open.php'; ?>
+<?php include 'templates/f_close.php'; ?>
+</body>
+</html>     
+        <!-- <h2>NFL</h2>
             <h3>NFC</h3>
                 <h4>East</h4>
                 <ul>
@@ -29,7 +251,7 @@ include 'templates/h_close.php';
                 </ul>
                 <h4>South</h4>
                 <ul>
-                    <li><a href="hhttps://www.nfl.com/teams/new-orleans-saints/" target="_blank"><img src="https://static.www.nfl.com/t_headshot_desktop/f_auto/league/api/clubs/logos/NO" width="100" height="85" alt="New Orleans Saints"/></a></li>
+                    <li><a href="https://www.nfl.com/teams/new-orleans-saints/" target="_blank"><img src="https://static.www.nfl.com/t_headshot_desktop/f_auto/league/api/clubs/logos/NO" width="100" height="85" alt="New Orleans Saints"/></a></li>
                     <li><a href="https://www.nfl.com/teams/tampa-bay-buccaneers/" target="_blank"><img src="https://static.www.nfl.com/t_headshot_desktop/f_auto/league/api/clubs/logos/TB" width="100" height="85" alt="Tampa Bay Buccaneers"/></a></li>
                     <li><a href="https://www.nfl.com/teams/carolina-panthers/" target="_blank"><img src="https://static.www.nfl.com/t_headshot_desktop/f_auto/league/api/clubs/logos/CAR" width="100" height="85" alt="Carolina Panthers"/></a></li>
                     <li><a href="https://www.nfl.com/teams/atlanta-falcons/" target="_blank"><img src="https://static.www.nfl.com/t_headshot_desktop/f_auto/league/api/clubs/logos/ATL" width="100" height="85" alt="Atlanta Falcons"/></a></li>
@@ -63,15 +285,6 @@ include 'templates/h_close.php';
                     <li><a href="https://www.nfl.com/teams/houston-texans/" target="_blank"><img src="https://static.www.nfl.com/t_headshot_desktop/f_auto/league/api/clubs/logos/HOU" width="100" height="85" alt="Houston Texans"/></a></li>
                     <li><a href="https://www.nfl.com/teams/jacksonville-jaguars/" target="_blank"><img src="https://static.www.nfl.com/t_headshot_desktop/f_auto/league/api/clubs/logos/JAX" width="100" height="85" alt="Jacksonville Jaguars"/></a></li>
                 </ul>
-        <h2>MLB</h2>
-            <h3>National League</h3>
-                <h4>East</h4>
-                <h4>Central</h4>
-                <h4>West</h4>               
-            <h3>American League</h3>
-                <h4>East</h4>
-                <h4>Central</h4>
-                <h4>West</h4> 
         <h2>NBA</h2>
             <h3>Eastern Conference</h3>
                 <h4>Atlantic</h4>
@@ -122,7 +335,7 @@ include 'templates/h_close.php';
                         <li><a href="https://www.nba.com/team/1610612740/pelicans" target="_blank"><img src="https://cdn.nba.com/logos/nba/1610612740/primary/L/logo.svg" width="100" height="85" alt="New Orleans Pelicans"/></a></li>
                         <li><a href="https://www.nba.com/team/1610612763/grizzlies" target="_blank"><img src="https://cdn.nba.com/logos/nba/1610612763/primary/L/logo.svg" width="100" height="85" alt="Memphis Grizzlies"/></a></li>
                         <li><a href="https://www.nba.com/team/1610612745/rockets" target="_blank"><img src="https://cdn.nba.com/logos/nba/1610612745/primary/L/logo.svg" width="100" height="85" alt="Houston Rockets"/></a></li>
-                    </ul>
+                    </ul> -->
 
 
 <?php include 'templates/aside.php'; ?>
