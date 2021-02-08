9cc2578fb3b6ffc70762ed5288fdd6b784b826e5
Kari Thomas <karithomas@Karis-MacBook-Air.local>
2021-02-08 10:42:01 -0500

    got some elements if footer fixed

--- templates/f_open.php
+++ templates/f_open.php
@@ -1,6 +1,6 @@
 <footer class="cell cell-4">
-    <!-- <div class="cell cell-5"> -->
-        <h2>Site Map</h2>
+    <div class="cell cell-5">
+        <h2 class="site-map">Site Map</h2>
         <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/sports-fandom.php">Sports Fandom</a></li>
@@ -8,20 +8,20 @@
             <li><a href="/sports-structure.php">League Info</a></li>
             <li><a href="/contact.php">Contact Me</a></li>
         </ul>
-    <!-- </div>
-    <div class="cell cell-6">     -->
+    </div>
+    <div class="cell cell-6">    
         <ul class="social-icons">
             <li><a href="https://www.twitter.com/FCFKariThomas" class="social-icons__link" target="_blank" aria-label="twitter"><i class="fa fa-twitter social-icons__icon"></i></a></li>
             <li><a href="https://www.instagram.com/kthomas720" class="social-icons__link" target="_blank" aria-label="instagram"><i class="fa fa-instagram social-icons__icon"></i></a></li>
             <li><a href="https://www.facebook.com/kari.thomas.12" class="social-icons__link" target="_blank" aria-label="facebook"><i class="fa fa-facebook social-icons__icon"></i></a></li>   
             <li><a href="https://www.youtube.com/channel/UCsKvfiJWxLq8qgzgH3aoS2g" class="social-icons__link" target="_blank" aria-label="youtube"><i class="fa fa-youtube social-icons__icon"></i></a></li>
         </ul>
-    <!-- </div>
-    <div class="cell cell-7"> -->
+    </div>
+    <div class="cell cell-7">
         <h2>Random Fact of the Day</h2>
         <ul>
             <li><a href="#">Random Fact of the Day</a></li>
         </ul>
-    <!-- </div> -->
+    </div>
     <small class="copyright">&copy; 2021 Kari Thomas</small>
 </footer>
