eedf47e8b8bf4a614ffe38e3be2d637940dfe854
Kari Thomas <karithomas@Karis-MacBook-Air.local>
2021-02-11 16:34:46 -0500

    fixed navbar and working on the footer

--- templates/h_close.php
+++ templates/h_close.php
@@ -2,14 +2,14 @@
 <body>
     <div class="main-container">
         <header class="grid top-nav">
-            <span class="title">Kari Thomas<br>FantasyPros Dev Trainee</span>
-            <input class="menu-btn" type="checkbox" id="menu-btn" />
-            <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-            <ul class="menu">
-                <li><a href="/">Home</a></li>
-                <li><a href="/sports-fandom.php">Sports Fandom</a></li>
-                <li><a href="/2020-fantasy-points.php">2020 Fantasy Points</a></li>
-                <li><a href="/sports-structure.php">League Info</a></li>
-                <li><a href="/contact.php">Contact Me</a></li>
+            <img class="logo" src="assets/images/logo.png" alt="FantasyPros Logo">
+            <nav>
+            <ul class="nav__links">
+                <li><a href="/">HOME</a></li>
+                <li><a href="/sports-fandom.php">SPORTS FANDOM</a></li>
+                <li><a href="/2020-fantasy-points.php">2020 FANTASY LEADERS</a></li>
+                <li><a href="/sports-structure.php">LEAGUE INFO</a></li>
+                <li><a href="/contact.php">CONTACT ME</a></li>
             </ul>
+            </nav>
         </header>
